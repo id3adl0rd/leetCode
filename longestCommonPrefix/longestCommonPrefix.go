aad30ef959530375cf33d164daf82d95c1830769
id3adl0rd <raindeadobzor2@gmail.com>
2024-06-23 20:49:20 +0300

    ex. 14

---
+++ longestCommonPrefix/longestCommonPrefix.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func longestCommonPrefix(strs []string) string {
+	sort.Strings(strs)
+
+	l := len(strs)
+	for i := range strs[0] {
+		if strs[0][i] != strs[l-1][i] {
+			return strs[0][:i]
+		}
+	}
+
+	return strs[0]
+}
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+}
